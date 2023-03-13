af5605845840c4d467a646e90884f4553710e102
johnabijujohn <109999866+johnabijujohn@users.noreply.github.com>
2023-03-13 23:50:30 +1030

    Update Referee.cpp

--- Referee.cpp
+++ Referee.cpp
@@ -11,78 +11,58 @@ Player* Referee::refGame(Player* player1, Player* player2) {
 
   if (playerMove1Name == "Rock") {
     if (playerMove2Name == "Scissors") {
-      Player* winner = player1;
       return player1;
     } else if (playerMove2Name == "Paper") {
-      Player* winner = player2;
       return player2;
     } else {
-      Player* winner = nullptr;
       return nullptr;
     }
   } else if (playerMove1Name == "Paper") {
     if (playerMove2Name == "Rock") {
-      Player* winner = player1;
       return player1;
     } else if (playerMove2Name == "Scissors") {
-      Player* winner = player2;
       return player2;
     } else {
-      Player* winner = nullptr;
       return nullptr;
     }
   } else if (playerMove1Name == "Scissors") {
     if (playerMove2Name == "Paper") {
-      Player* winner = player1;
       return player1;
     } else if (playerMove2Name == "Rock") {
-      Player* winner = player2;
       return player2;
     } else {
-      Player* winner = nullptr;
       return nullptr;
     }
   } else if (playerMove1Name == playerMove2Name) {
-    Player* winner = nullptr;
     return nullptr;
   } else if (playerMove1Name == "Monkey") {
     if (playerMove2Name == "Ninja" || playerMove2Name == "Robot") {
-      Player* winner = player1;
       return player1;
     } else {
-      Player* winner = player2;
       return player2;
     };
   } else if (playerMove1Name == "Robot") {
     if (playerMove2Name == "Ninja" || playerMove2Name == "Zombie") {
-      Player* winner = player1;
       return player1;
     } else {
-      Player* winner = player2;
       return player2;
     };
   } else if (playerMove1Name == "Pirate") {
     if (playerMove2Name == "Robot" || playerMove2Name == "Monkey") {
-      Player* winner = player1;
       return player1;
     } else {
-      Player* winner = player2;
       return player2;
     };
   } else if (playerMove1Name == "Ninja") {
     if (playerMove2Name == "Pirate" || playerMove2Name == "Zombie") {
-      Player* winner = player1;
       return player1;
     } else {
-      Player* winner = player2;
       return player2;
     };
   } else if (playerMove1Name == "Zombie") {
     if (playerMove2Name == "Pirate" || playerMove2Name == "Monkey") {
-      Player* winner = player1;
       return player1;
     } else {
-      Player* winner = player2;
       return player2;
     };
   }
