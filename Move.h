755ce91934711f92455a9beff52174170c83db50
johnabijujohn <109999866+johnabijujohn@users.noreply.github.com>
2023-03-13 23:55:54 +1030

    Update Move.h

--- Move.h
+++ Move.h
@@ -10,34 +10,58 @@ class Move {
 
 class Rock : public Move {
  public:
-  std::string getName() { return "Rock"; };
+  std::string getName() {
+    std::cout << "Rock" << std::endl;
+    return "Rock";
+  };
 };
 class Paper : public Move {
  public:
-  std::string getName() { return "Paper"; };
+  std::string getName() {
+    std::cout << "Paper" << std::endl;
+    return "Paper";
+  };
 };
 class Scissors : public Move {
  public:
-  std::string getName() { return "Scissors"; };
+  std::string getName() {
+    std::cout << "Scissors" << std::endl;
+    return "Scissors";
+  };
 };
 class Robot : public Move {
  public:
-  std::string getName() { return "Robot"; };
+  std::string getName() {
+    std::cout << "Robot" << std::endl;
+    return "Robot";
+  };
 };
 class Monkey : public Move {
  public:
-  std::string getName() { return "Monkey"; };
+  std::string getName() {
+    std::cout << "Monkey" << std::endl;
+    return "Monkey";
+  };
 };
 class Pirate : public Move {
  public:
-  std::string getName() { return "Pirate"; };
+  std::string getName() {
+    std::cout << "Pirate" << std::endl;
+    return "Pirate";
+  };
 };
 class Ninja : public Move {
  public:
-  std::string getName() { return "Ninja"; };
+  std::string getName() {
+    std::cout << "Ninja" << std::endl;
+    return "Ninja";
+  };
 };
 class Zombie : public Move {
  public:
-  std::string getName() { return "Zombie"; };
+  std::string getName() {
+    std::cout << "Zombie" << std::endl;
+    return "Zombie";
+  };
 };
 #endif
