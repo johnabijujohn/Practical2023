3cdaab01e7eba21ae813f0a1a42a96dd3ab363c7
johnabijujohn <109999866+johnabijujohn@users.noreply.github.com>
2023-05-15 12:33:21 +0930

    Update Autocomplete.cpp

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -9,7 +9,7 @@ void Autocomplete::insert(std::string word) {
     TrieNode* currentNode = root;
 
     for (char c : word) {
-    int in = c - 'a'; // Assuming lowercase English alphabet
+    int in = c - 'a';
         if (currentNode->children[in] == nullptr) {
             currentNode->children[in] = new TrieNode();
         }
@@ -18,3 +18,34 @@ void Autocomplete::insert(std::string word) {
 
     currentNode->isEndOfWord = true;
 }
+
+std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord) {
+    TrieNode* currentNode = root;
+    std::vector<std::string> suggestions;
+    for (char c : partialWord) {
+      int in = c - 'a';
+      if (currentNode->children[in] == nullptr) {
+        return suggestions;  // empty vector
+      }
+      currentNode = currentNode->children[in];
+    }
+
+    getWords(partialWord, currentNode, suggestions);
+
+    return suggestions;  // Return true if the partial word is found in the trie
+  }
+
+void Autocomplete::getWords(std::string builtWord, TrieNode* current, std::vector<std::string>& suggestions) {
+    if (current->isEndOfWord == true) {
+      suggestions.push_back(builtWord);
+    }
+
+    // loop through all the alphabets
+    for (int i = 0; i < 26; i++) {
+      if (current->children[i] != nullptr) { // if a entry is not nullptr that means there is a valid letter
+        builtWord.push_back('a' + i); // add valid letter to the back of current word
+        getWords(builtWord, current->children[i], suggestions); // call function again but with the next child
+      }
+    }
+}
+
