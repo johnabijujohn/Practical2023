02f94fe7e1e1ee5ba98c94531711e9200dd479f8
johnabijujohn <109999866+johnabijujohn@users.noreply.github.com>
2023-05-15 12:33:34 +0930

    Update Autocomplete.h

--- Autocomplete.h
+++ Autocomplete.h
@@ -1,32 +1,31 @@
 #ifndef AUTOCOMPLETE_H
 #define AUTOCOMPLETE_H
 
-#include <vector>
 #include <string>
-
+#include <vector>
 
 class Autocomplete {
-    private:
-        struct TrieNode{
-            std::vector<TrieNode *> children;
-            bool isEndOfWord;
+ private:
+  struct TrieNode {
+    std::vector<TrieNode*> children;
+    bool isEndOfWord;
 
-            TrieNode() {
-                isEndOfWord = false;
-                children = std::vector<TrieNode*>(26, nullptr);
-            }
-        };
-
-        TrieNode *root;
-
-    public:
-    Autocomplete();
-    void insert(std::string word);
-
-    std::vector<std::string> getSuggestions(std::string partialWord) {
-        std::vector<std::string> suggestion;
-        return suggestion;
+    TrieNode() {
+      isEndOfWord = false;
+      children = std::vector<TrieNode*>(26, nullptr);
     }
+  };
+
+  TrieNode* root;
+
+ public:
+  Autocomplete();
+
+  void insert(std::string word);
+
+  std::vector<std::string> getSuggestions(std::string partialWord);
+
+  void getWords(std::string builtWord, TrieNode* current, std::vector<std::string>& suggestions);
 };
 
 #endif
